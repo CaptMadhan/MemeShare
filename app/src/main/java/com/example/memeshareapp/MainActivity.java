97b25c8f09afaa4da580464c8c0b6e755e4b0828
Madhan M <madhanmandy3374@gmail.com>
2021-06-06 21:07:56 +0530

    Optimized

--- app/src/main/java/com/example/memeshareapp/MainActivity.java
+++ app/src/main/java/com/example/memeshareapp/MainActivity.java
@@ -1,25 +1,17 @@
 package com.example.memeshareapp;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
-
 import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     ImageView memeImageView;
@@ -28,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         memeImageView = (ImageView)findViewById(R.id.memeImageView);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
     loadmeme();
     }
     private void loadmeme(){
@@ -36,24 +28,14 @@ public class MainActivity extends AppCompatActivity {
         String url ="https://meme-api.herokuapp.com/gimme";
         // Request a string response from the provided URL.
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            String url = response.getString("url");
-                            Glide.with(MainActivity.this).load(url).into(memeImageView);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                (Request.Method.GET, url, null, response -> {
+                    try {
+                        String url1 = response.getString("url");
+                        Glide.with(MainActivity.this).load(url1).into(memeImageView);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity.this,"Something went wrong", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                }, error -> Toast.makeText(MainActivity.this,"Something went wrong", Toast.LENGTH_SHORT).show());
         Volley.newRequestQueue(MainActivity.this).add(jsonObjectRequest);
     }
 
