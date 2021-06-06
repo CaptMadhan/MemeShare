91245c53dceb34079c5c476f2f91111cba5b79ae
Madhan M <madhanmandy3374@gmail.com>
2021-06-06 16:58:23 +0530

    FrontEnd

--- app/src/main/java/com/example/memeshareapp/MainActivity.java
+++ app/src/main/java/com/example/memeshareapp/MainActivity.java
@@ -4,13 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView memeImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        memeImageView = (ImageView)findViewById(R.id.memeImageView);
+    loadmeme();
+    }
+    private void loadmeme(){
+        // Instantiate the RequestQueue.
+        String url ="https://meme-api.herokuapp.com/gimme";
+        // Request a string response from the provided URL.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String url = response.getString("url");
+                            Glide.with(MainActivity.this).load(url).into(memeImageView);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,"Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        Volley.newRequestQueue(MainActivity.this).add(jsonObjectRequest);
     }
 
     public void shareMeme(View view) {
