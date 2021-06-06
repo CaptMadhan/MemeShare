9bec2607235a70ec96326e8e8fc86132d3dc554f
Madhan M <madhanmandy3374@gmail.com>
2021-06-06 21:59:26 +0530

    Added share functionality using intent

--- app/src/main/java/com/example/memeshareapp/MainActivity.java
+++ app/src/main/java/com/example/memeshareapp/MainActivity.java
@@ -1,37 +1,66 @@
 package com.example.memeshareapp;
 
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
 import org.json.JSONException;
+import org.w3c.dom.Text;
+
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     ImageView memeImageView;
+    ProgressBar loadImage;
+    String current_url = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         memeImageView = (ImageView)findViewById(R.id.memeImageView);
+        loadImage = (ProgressBar)findViewById(R.id.imageloader);
         Objects.requireNonNull(getSupportActionBar()).hide();
     loadmeme();
     }
     private void loadmeme(){
         // Instantiate the RequestQueue.
-        String url ="https://meme-api.herokuapp.com/gimme";
+        current_url ="https://meme-api.herokuapp.com/gimme";
         // Request a string response from the provided URL.
+        loadImage.setVisibility(View.VISIBLE);
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, response -> {
+                (Request.Method.GET, current_url, null, response -> {
                     try {
                         String url1 = response.getString("url");
-                        Glide.with(MainActivity.this).load(url1).into(memeImageView);
+                        Glide.with(MainActivity.this).load(url1).listener(new RequestListener<Drawable>(){
+
+                            @Override
+                            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                loadImage.setVisibility(View.GONE);
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                loadImage.setVisibility(View.GONE);
+                                return false;
+                            }
+                        }).into(memeImageView);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
@@ -40,6 +69,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void shareMeme(View view) {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,"Hey, checkout this meme I got from Reddit "+current_url+" ");
+        Intent chooser = Intent.createChooser(shareIntent,"Share this meme using...");
+        startActivity(chooser);
     }
 
     public void nextMeme(View view) {
